5e212ba470210ab106c17e9641af8b918e444923
Oleg Avdyeyev <avdeevoleg91@gmail.com>
2018-10-10 18:14:56 +0300

    v12

---
+++ OnlineStore/Controllers/CategoriesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnlineStore.Models;
+
+namespace OnlineStore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private OnlineStoreContext db = new OnlineStoreContext();
+
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+
+        // GET: Categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+       
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- OnlineStore/Models/OnlineStoreContext.cs
+++ OnlineStore/Models/OnlineStoreContext.cs
@@ -6,10 +6,10 @@ using System.Web;
 
 namespace OnlineStore.Models
 {
-    public class OnlineStoreContext
+    public class OnlineStoreContext : DbContext
     {      
             public DbSet<Category> Categories { get; set; }
-            public DbSet<Customer> ustomers { get; set; }
+            public DbSet<Customer> Customers { get; set; }
             public DbSet<Product> Products { get; set; }
             public DbSet<Purchase> Purchases { get; set; }
             public DbSet<ShoppingCart> ShoppingCarts { get; set; }
